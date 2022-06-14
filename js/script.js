681dce3db31159619351919d05d05b169ad2dd09
kyla42 <103237579+kyla42@users.noreply.github.com>
2022-06-15 00:51:18 +0900

    change color

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const bulb = document.querySelector(".bulb-area");
-const bulbColor = ["on-yellow", "on-orange", "on-pink", "on-white", "on-lightblue", "on-plum", "on-lightgreen"];
+const bulbColor = ["on-yellow", "on-orange", "on-pink", "on-white", "on-lightblue", "on-mediumorchid", "on-lightgreen"];
 
 bulb.addEventListener("mouseover", function(){
     const randomC = bulbColor[Math.floor(Math.random() * bulbColor.length)];
