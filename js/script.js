c368680037736eeb3edfca7ce41d53b5941f9a50
kyla42 <103237579+kyla42@users.noreply.github.com>
2022-06-15 00:45:52 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const bulb = document.querySelector(".bulb-area");
+const bulbColor = ["on-yellow", "on-orange", "on-pink", "on-white", "on-lightblue", "on-plum", "on-lightgreen"];
+
+bulb.addEventListener("mouseover", function(){
+    const randomC = bulbColor[Math.floor(Math.random() * bulbColor.length)];
+
+    bulb.classList.add(randomC);
+
+    bulb.addEventListener("mouseout", function(){
+        bulb.classList.remove(randomC);     
+    });
+});
